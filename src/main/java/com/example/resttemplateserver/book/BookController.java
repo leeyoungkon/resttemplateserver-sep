c10bd1e89eaba006eda39ffc8dc75ccf6aeeb2d4
rkgj4106 <rkgj0426@gmail.com>
2026-08-03 01:06:58 +0900

    18th commit

--- src/main/java/com/example/resttemplateserver/book/BookController.java
+++ src/main/java/com/example/resttemplateserver/book/BookController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NOT_FOUND;  
 
 @RestController
 @RequestMapping("/books")
